cf25661dfebc19bb9620fc2e0527a89834a3d238
Kevin Clark <clar0737@flinders.edu.au>
2014-04-03 14:44:52 +1030

    Passed Distinct Value Combination test

--- tilt.c
+++ tilt.c
@@ -7,7 +7,15 @@ int tilt_line_left(int length,int *line)
   if (length<1||length>255) return -1;
 
   // slide tiles to the left
-
+int x,y;
+for (y=1;y<length; y++){
+	for (x=1; x<length; x++){
+		if (line[x-1] == 0){
+			line [x-1] = line [x];
+			line [x] = 0;
+		}
+	}
+}
   // combine tiles as required
 
   return 0;
