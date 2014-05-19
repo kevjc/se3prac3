8ff16b3d56e0f654caedab3a5b9293340860ee12
Kevin Clark <clar0737@flinders.edu.au>
2014-05-19 09:40:26 +0930

    added tilt right tests

--- test.c
+++ test.c
@@ -43,7 +43,23 @@ int test_tilt_left()
   e|=ttl_vector(1,1,1,0,"Cominations do occur from left when tilting from left.",2,1,0,0);
   e|=ttl_vector(0,0,1,1,"Combinations occur on shift",2,0,0,0);
   e|=ttl_vector(4,0,1,1,"Combinations occur on partial shift",4,2,0,0);
-  e|=ttl_vector(2,0,1,1,Single combination does not cascade",2,2,0,0);
+  e|=ttl_vector(2,0,1,1,"Single combination does not cascade",2,2,0,0);
+  return e;
+}
+
+int test_tilt_right()
+{
+  int e=0;
+  e|=ttr_vector(0,0,0,0,"Empty list is empty after shift",0,0,0,0);
+  e|=ttr_vector(0,0,0,1,"Value on right stays on right after shift",0,0,0,1);
+  e|=ttr_vector(1,0,0,0,"Value on left shifts to right edge after shift",0,0,0,1);
+  e|=ttr_vector(0,0,1,0,"Value in middle shifts to right edge after shift",0,0,0,1);
+  e|=ttr_vector(1,2,4,8,"Distinct values don't combine",1,2,4,8);
+  e|=ttr_vector(1,1,1,1,"Combinations don't cascade",0,0,2,2);
+  e|=ttr_vector(1,1,1,0,"Cominations do occur from left when tilting from left.",0,0,1,2);
+  e|=ttr_vector(0,0,1,1,"Combinations occur on shift",0,0,0,2);
+  e|=ttr_vector(4,0,1,1,"Combinations occur on partial shift",0,0,2,4);
+  e|=ttr_vector(2,0,1,1,"Single combination does not cascade",0,0,2,2);
   return e;
 }
 
@@ -51,5 +67,6 @@ int main(int argc,char **argv)
 {
   int e=0;
   e|=test_tilt_left();
+  e|=test_tilt_right();
   return e;
 }
