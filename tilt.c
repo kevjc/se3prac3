fe3df2cd113d7c9c185da2e35152f4c7213be155
Kevin Clark <clar0737@flinders.edu.au>
2014-05-19 09:32:27 +0930

    fixed problems with shift cascading

--- tilt.c
+++ tilt.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include "2048.h"
 
- // slide tiles to the left
-int slide (int length, int *line)
+//Main
+int tilt_line_left(int length,int *line)
 {
+  // make sure vector length is sensible
+  if (length<1||length>255) return -1;
+
 int y,x;
 for (y=1;y<length; y++){
 	for (x=1; x<length; x++){
@@ -13,34 +16,20 @@ for (y=1;y<length; y++){
 		}
 	}
 }
-return 0;
-}
-
- // combine tiles as required
-int combine (int length, int *line)
-{
-int x,y;
-for (y=1;y<length; y++){
 	for (x=1; x<length; x++){
 		if (line[x-1] == line [x]){
 			line [x-1] = 2*line [x];
 			line [x] = 0;
-			x++;
+		}
+	}
+for (y=1;y<length; y++){
+	for (x=1; x<length; x++){
+		if (line[x-1] == 0){
+			line [x-1] = line [x];
+			line [x] = 0;
 		}
 	}
 }
-return 0;
-}
-
-//Main
-int tilt_line_left(int length,int *line)
-{
-  // make sure vector length is sensible
-  if (length<1||length>255) return -1;
-
-slide(length, line);
-combine (length, line);
-slide(length, line);
   return 0;
 }
 
