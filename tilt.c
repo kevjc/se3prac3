1ac14d0b2136a9592d104c3b0455ef38a7dc464b
Kevin Clark <clar0737@flinders.edu.au>
2014-05-19 09:35:40 +0930

    added tilt right attempt1

--- tilt.c
+++ tilt.c
@@ -33,4 +33,34 @@ for (y=1;y<length; y++){
   return 0;
 }
 
+int tilt_line_right(int length,int *line)
+{
+  // make sure vector length is sensible
+  if (length<1||length>255) return -1;
+
+int y,x;
+for (y=length-2;y>0; y--){
+	for (x=length-2; x>0; x--){
+		if (line[x+1] == 0){
+			line [x+1] = line [x];
+			line [x] = 0;
+		}
+	}
+}
+for (x=length-2; x>0; x--){
+	if (line[x+1] == line [x]){
+		line [x+1] = 2*line [x];
+		line [x] = 0;
+	}
+}
+for (y=length-2;y>0; y--){
+	for (x=length-2; x>0; x--){
+		if (line[x+1] == 0){
+			line [x+1] = line [x];
+			line [x] = 0;
+		}
+	}
+}
+  return 0;
+}
 
