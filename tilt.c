17a9c926cbaee5f73a1330fc4cf7523d42ddb6f2
Kevin Clark <clar0737@flinders.edu.au>
2014-06-03 14:02:15 +0930

    Fixed variable initialisation mistake

--- tilt.c
+++ tilt.c
@@ -66,11 +66,11 @@ for (y=length-2;y>=0; y--){
 
 int tilt_board_right(int size, int **board){
 // make sure board size is sensible
-  if (length<1||length>255) return -1;
+  if (size<1||size>255) return -1;
 	int x=0;
 	int y=0;
 	for(x;x< size/2;x++){
-		for(y;y<size/2;y++)
+		for(y;y<size/2;y++){
 	int temp = board[x+y][x+y];
 	board[x+y][x+y]= board[size-(x+y)][x+y];
 	board[size-(x+y)][x+y] = board[size-(x+y)][size-(x+y)];
