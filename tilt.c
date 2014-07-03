821d10c33084d9e4bb4b70145df57dac5a372bc4
Kevin Clark <clar0737@flinders.edu.au>
2014-07-03 12:33:51 +0930

    imrpived 3*3 and 4*4 behavior- still doesn't work for outer ring

--- tilt.c
+++ tilt.c
@@ -66,42 +66,17 @@ for (y=length-2;y>=0; y--){
 
 int board_rotate_right(int size, int **board){
 // make sure board size is sensible
-printf("not a loop\n");
-fflush(stdout);
   if (size<1||size>255) return -1;
 	int x=0;
 	int y=0;
     int temp=0;
-//for 2*2 tests
-	printf("%d,%d\n%d,%d\n",board[0][0], board[0][1],board[1][0],board[1][1]); 
-	for(x=0;x<size/2;x++){
-		printf("outest loop\n");
-		fflush(stdout);
+	for(x=0;x<=size/2;x++){
 		for(y=0;y<size-x-1;y++){
-			printf("innest loop\n");
-			printf("x=%d, y=%d, size = %d \n", x,y,size);
-			fflush(stdout);
 			temp = board[x+y][x+y];
-			printf("%d,%d\n%d,%d\n",board[0][0], board[0][1],board[1][0],board[1][1]); 
-			fflush(stdout);
-			printf("x=%d, y=%d, size = %d \n", x,y,size);
-			fflush(stdout);
 			board[x+y][x+y]= board[size-(x+y)-1][x+y];
-			printf("x=%d, y=%d, size = %d \n", x,y,size);
-			printf("%d,%d\n%d,%d\n",board[0][0], board[0][1],board[1][0],board[1][1]); 
-			fflush(stdout);
 			board[size-(x+y)-1][x+y] = board[size-(x+y)-1][size-(x+y)-1];
-			printf("x=%d, y=%d, size = %d \n", x,y,size);
-			printf("%d,%d\n%d,%d\n",board[0][0], board[0][1],board[1][0],board[1][1]); 
-			fflush(stdout);
 			board[size-(x+y)-1][size-(x+y)-1] = board[x+y][size-(x+y)-1];
-			printf("x=%d, y=%d, size = %d \n", x,y,size);
-			printf("%d,%d\n%d,%d\n",board[0][0], board[0][1],board[1][0],board[1][1]); 
-			fflush(stdout);
 			board[x+y][size-(x+y)-1] = temp;
-			printf("x=%d, y=%d, size = %d \n", x,y,size);
-			printf("%d,%d\n%d,%d\n",board[0][0], board[0][1],board[1][0],board[1][1]); 
-			fflush(stdout);
 }
 }		
 return 0;
