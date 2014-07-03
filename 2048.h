90e2f217f3e59b08b6c15a6e9b69d9e81ea30c89
Kevin Clark <clar0737@flinders.edu.au>
2014-07-04 00:13:55 +0930

    final commit to try and solve git merge problems

--- 2048.h
+++ 2048.h
@@ -7,10 +7,6 @@ int read_input();
 #define GO_NOWHERE 0
 #define INVALID_INPUT -1
 
-int tilt_board_left(int size,int **board);
-int tilt_board_right(int size,int **board);
-int tilt_board_up(int size,int **board);
-int tilt_board_down(int size,int **board);
 int **board_create(int size);
 int board_display(int size,int **board);
 int board_spawn_tile(int size,int **board);
@@ -22,4 +18,5 @@ int board_rotate_right(int size, int **board);
 int board_rotate_left(int size, int **board);
 int tilt_board_up(int size, int **board);
 int tilt_board_down(int size, int **board);
-
+int tilt_board_left(int size,int **board);
+int tilt_board_right(int size,int **board);
