5585f03fc4d53775c5ba9bd13011c91c27c8f1d8
Kevin Clark <clar0737@flinders.edu.au>
2014-07-03 16:41:16 +0930

    added up basic up tilt test

--- test.c
+++ test.c
@@ -223,6 +223,14 @@ int test_board_rotations()
 																  		5,6,7,8,
 																  	    9,10,11,12,
 																   		13,14,15,16,board_rotate_left);
+  e|=board4_vector_test(0,0,0,0,
+						0,0,0,0,
+						0,0,0,0,
+						1,1,1,1,"(4*4)Tilt up Single Value",1,1,1,1,
+														   	0,0,0,0,
+														   	0,0,0,0,
+														   	0,0,0,0,board_rotate_right);
+
 return e;
 }
 
