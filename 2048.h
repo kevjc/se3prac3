1df303653aae8ccae5d669cd48ccde0e678378f9
Kevin Clark <clar0737@flinders.edu.au>
2014-07-03 16:44:05 +0930

    added up basic up tilt

--- 2048.h
+++ 2048.h
@@ -20,4 +20,5 @@ int tilt_line_left(int length,int *line);
 int tilt_line_right(int length,int *line);
 int board_rotate_right(int size, int **board);
 int board_rotate_left(int size, int **board);
+int tilt_board_up(int size, int **board);
 
